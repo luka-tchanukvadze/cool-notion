6300eeb1749489cf94816708a618fd40a3fc8939
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2023-09-25 16:01:29 +0400

    working on editor

--- src/components/ForNotes/Editor.js
+++ src/components/ForNotes/Editor.js
@@ -1,155 +1,80 @@
 import ReactMarkdown from "react-markdown";
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 
 function Editor(props) {
   if (!props.currentNote)
     return <div className="no-active-note">No Active Note</div>;
 
-  console.log(props.currentNote.body);
+  const [editing, setEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(props.currentNote.body);
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    if (editing) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [editing]);
+
+  const handleOutsideClick = (e) => {
+    if (textareaRef.current && !textareaRef.current.contains(e.target)) {
+      // props.updateNote(editedContent);
+      setEditing(false);
+    }
+  };
+
+  const toggleEditMode = () => {
+    setEditing(!editing);
+  };
+
+  const handleContentChange = (e) => {
+    setEditedContent(e.target.value);
+  };
+
+  const handleSave = () => {
+    // Save the edited content and exit editing mode
+    props.updateNote(editedContent);
+    setEditing(false);
+  };
+
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <textarea
-          id="body"
-          placeholder="Write your note here..."
-          value={props.currentNote.body}
-          onChange={(e) => {
-            props.updateNote(e.target.value);
-          }}
-        />
+        {editing ? (
+          <textarea
+            style={{ width: "300px", height: "100px" }}
+            ref={textareaRef} // Assign the ref to the textarea element
+            placeholder="Write your note here..."
+            value={props.currentNote.body}
+            onChange={(e) => {
+              props.updateNote(e.target.value);
+            }}
+            onClick={handleOutsideClick}
+          />
+        ) : (
+          // <div onClick={toggleEditMode}>{props.currentNote.body}</div>
+          <div onClick={toggleEditMode} className="app-main-note-preview">
+            <h1 className="preview-title">{props.currentNote.title}</h1>
+            <ReactMarkdown className="markdown-preview">
+              {props.currentNote.body}
+            </ReactMarkdown>
+          </div>
+        )}
       </div>
-      <div className="app-main-note-preview">
+      {/* <div className="app-main-note-preview">
         <h1 className="preview-title">{props.currentNote.title}</h1>
         <ReactMarkdown className="markdown-preview">
           {props.currentNote.body}
         </ReactMarkdown>
-      </div>
+      </div> */}
     </div>
   );
 }
-
-// import React from "react";
-// import ReactMde from "react-mde";
-// import ReactMarkdown from "react-markdown";
-// import Showdown from "showdown";
-// // import "react-mde/lib/styles/css/react-mde-all.css";
-
-// function loadSuggestions(text) {
-//   return new Promise((accept, reject) => {
-//     setTimeout(() => {
-//       const suggestions = [
-//         {
-//           preview: "Andre",
-//           value: "@andre",
-//         },
-//         {
-//           preview: "Angela",
-//           value: "@angela",
-//         },
-//         {
-//           preview: "David",
-//           value: "@david",
-//         },
-//         {
-//           preview: "Louise",
-//           value: "@louise",
-//         },
-//       ].filter((i) => i.preview.toLowerCase().includes(text.toLowerCase()));
-//       accept(suggestions);
-//     }, 250);
-//   });
-// }
-
-// function Editor(props) {
-//   const [selectedTab, setSelectedTab] = React.useState("write");
-
-//   const save = async function*(data) {
-//     // Promise that waits for "time" milliseconds
-//     const wait = function(time) {
-//       return new Promise((a, r) => {
-//         setTimeout(() => a(), time);
-//       });
-//     };
-
-//     // Upload "data" to your server
-//     // Use XMLHttpRequest.send to send a FormData object containing
-//     // "data"
-//     // Check this question: https://stackoverflow.com/questions/18055422/how-to-receive-php-image-data-over-copy-n-paste-javascript-with-xmlhttprequest
-
-//     await wait(2000);
-//     // yields the URL that should be inserted in the markdown
-//     yield "https://picsum.photos/300";
-//     await wait(2000);
-
-//     // returns true meaning that the save was successful
-//     return true;
-//   };
-
-//   const converter = new Showdown.Converter({
-//     tables: true,
-//     simplifiedAutoLink: true,
-//     strikethrough: true,
-//     tasklists: true,
-//   });
-
-//   return (
-//     <section className="pane editor">
-//       <ReactMde
-//         value={props.currentNote.body}
-//         onChange={props.updateNote}
-//         selectedTab={selectedTab}
-//         onTabChange={setSelectedTab}
-//         generateMarkdownPreview={(markdown) =>
-//           Promise.resolve(<ReactMarkdown source={markdown} />)
-//         }
-//         loadSuggestions={loadSuggestions}
-//         childProps={{
-//           writeButton: {
-//             tabIndex: -1,
-//           },
-//         }}
-//         paste={{
-//           saveImage: save,
-//         }}
-//       />
-//     </section>
-//   );
-// }
-
-// const onEditField = (field, value) => {
-//   onUpdateNote({
-//     ...activeNote,
-//     [field]: value,
-//     lastModified: Date.now(),
-//   });
-// };
-
-// if (!activeNote) return <div className="no-active-note">No Active Note</div>;
-
-// return (
-//   <div className="app-main">
-//     <div className="app-main-note-edit">
-//       <input
-//         type="text"
-//         id="title"
-//         placeholder="Note Title"
-//         value={activeNote.title}
-//         onChange={(e) => onEditField("title", e.target.value)}
-//         autoFocus
-//       />
-//       <textarea
-//         id="body"
-//         placeholder="Write your note here..."
-//         value={activeNote.body}
-//         onChange={(e) => onEditField("body", e.target.value)}
-//       />
-//     </div>
-//     <div className="app-main-note-preview">
-//       <h1 className="preview-title">{activeNote.title}</h1>
-//       <ReactMarkdown className="markdown-preview">
-//         {activeNote.body}
-//       </ReactMarkdown>
-//     </div>
-//   </div>
 
 export default Editor;
