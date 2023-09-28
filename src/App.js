7b12684e5bd1aa67a6c701be2cac2b8711773b64
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2023-09-28 14:51:54 +0400

    removing hardcoded css

--- src/App.js
+++ src/App.js
@@ -1,145 +1,3 @@
-// import * as React from "react";
-// import "react-mde/lib/styles/css/react-mde-all.css";
-// import Editor from "./components/ForNotes/Editor";
-// import Sidebar from "./components/ForNotes/Sidebar";
-// import CreateNotes from "./components/CreateNotes";
-// // import Navigation from "./components/Navigation";
-// import Header from "./components/Header";
-// import Bin from "./components/menus/Bin";
-// import { v4 as uuidv4 } from "uuid";
-// import Split from "react-split";
-// import { useState, useEffect } from "react";
-// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import FuturePlans from "./components/Schedule/FuturePlans";
-// // import { Button } from "react-bootstrap";
-// // import "bootstrap/dist/css/bootstrap.min.css";
-
-// export default function App() {
-//   const [notes, setNotes] = useState(
-//     () => JSON.parse(localStorage.getItem("notes")) || []
-//   );
-
-//   const [currentNoteId, setCurrentNoteId] = useState(
-//     (notes[0] && notes[0].id) || ""
-//   );
-
-//   useEffect(() => {
-//     localStorage.setItem("notes", JSON.stringify(notes));
-//   }, [notes]);
-
-//   function createNewNote() {
-//     const newNote = {
-//       id: uuidv4(),
-//       body: "# Type your markdown note's title here",
-//       // textAreas: [],
-//     };
-//     setNotes((prevNotes) => [newNote, ...prevNotes]);
-//     setCurrentNoteId(newNote.id);
-//   }
-
-//   function updateNote(text) {
-//     // Put the most recently-modified note at the top
-//     setNotes((oldNotes) => {
-//       const newArray = [];
-//       for (let i = 0; i < oldNotes.length; i++) {
-//         const oldNote = oldNotes[i];
-//         if (oldNote.id === currentNoteId) {
-//           newArray.unshift({ ...oldNote, body: text });
-//           // newArray.unshift({ ...oldNote, body: text, textAreas: [text] });
-//         } else {
-//           newArray.push(oldNote);
-//         }
-//       }
-//       return newArray;
-//     });
-//   }
-
-//   function deleteNote(event, noteId) {
-//     event.stopPropagation();
-//     setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
-//   }
-
-//   function findCurrentNote() {
-//     return (
-//       notes.find((note) => {
-//         return note.id === currentNoteId;
-//       }) || notes[0]
-//     );
-//   }
-
-//   const [darkMode, setDarkMode] = useState(false);
-
-//   function toggleDarkMode() {
-//     setDarkMode((prevMode) => !prevMode);
-//   }
-
-//   return (
-//     <BrowserRouter>
-//       <main
-//         className={`${darkMode ? "dark" : ""}`}
-//         style={{ backgroundColor: darkMode ? "#191D1D" : "" }}
-//       >
-//         {notes.length > 0 ? (
-//           <Split sizes={[30, 70]} direction="horizontal" className="split">
-//             <div className="nav-container">
-//               <Sidebar
-//                 notes={notes}
-//                 currentNote={findCurrentNote()}
-//                 setCurrentNoteId={setCurrentNoteId}
-//                 newNote={createNewNote}
-//                 deleteNote={deleteNote}
-//               />
-
-//               {/* <Navigation /> */}
-//               {/* <Bin /> */}
-//             </div>
-//             {currentNoteId && notes.length > 0 && (
-//               <div className="header-container">
-//                 <Header
-//                   currentNote={findCurrentNote()}
-//                   darkMode={darkMode}
-//                   setDarkMode={setDarkMode}
-//                   toggleDarkMode={toggleDarkMode}
-//                 />
-//                 <Routes>
-//                   <Route
-//                     path="/"
-//                     element={
-//                       <Editor
-//                         currentNote={findCurrentNote()}
-//                         updateNote={updateNote}
-//                       />
-//                     }
-//                   />
-//                   <Route path="/futurePlans" element={<FuturePlans />} />
-//                 </Routes>
-//               </div>
-//             )}
-//           </Split>
-//         ) : (
-//           <Split sizes={[50, 50]} direction="horizontal" className="split">
-//             <div style={{ marginTop: "7.5rem" }} className="nav-container-2">
-//               <FuturePlans />
-//             </div>
-
-//             <div className="editor-side">
-//               <div className="header-container-2">
-//                 <Header
-//                   currentNote={findCurrentNote()}
-//                   darkMode={darkMode}
-//                   setDarkMode={setDarkMode}
-//                   toggleDarkMode={toggleDarkMode}
-//                 />
-//               </div>
-
-//               <CreateNotes createNewNote={createNewNote} />
-//             </div>
-//           </Split>
-//         )}
-//       </main>
-//     </BrowserRouter>
-//   );
-// }
 import * as React from "react";
 import "react-mde/lib/styles/css/react-mde-all.css";
 import Editor from "./components/ForNotes/Editor";
@@ -155,6 +13,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import FuturePlans from "./components/Schedule/FuturePlans";
 // import { Button } from "react-bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
+import reactTextareaAutosize from "react-textarea-autosize";
 
 export default function App() {
   const [notes, setNotes] = useState(
@@ -181,8 +40,9 @@ export default function App() {
 
   const updateTextarea = (textAreas) => {
     setNotes((oldNotes) =>
-      oldNotes.map((note) =>
-        note.id === currentNoteId ? { ...note, textAreas } : note
+      oldNotes.map(
+        (note) => (note.id === currentNoteId ? { ...note, textAreas } : note)
+        // note.id === currentNoteId && { ...note, textAreas }
       )
     );
   };
@@ -227,7 +87,10 @@ export default function App() {
     <BrowserRouter>
       <main
         className={`${darkMode ? "dark" : ""}`}
-        style={{ backgroundColor: darkMode ? "#191D1D" : "" }}
+        style={{
+          backgroundColor: darkMode ? "#191D1D" : "",
+          transition: "0.20s",
+        }}
       >
         {notes.length > 0 ? (
           <Split sizes={[30, 70]} direction="horizontal" className="split">
@@ -259,6 +122,9 @@ export default function App() {
                         updateTextarea={updateTextarea}
                         currentNote={findCurrentNote()}
                         updateNote={updateNote}
+                        rows={5}
+                        minRows={5}
+                        maxRows={10}
                       />
                     }
                   />
--- src/components/ForNotes/Editor.js
+++ src/components/ForNotes/Editor.js
@@ -4,6 +4,7 @@ import { useState, useRef, useCallback, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import TextareaAutosize from "react-textarea-autosize";
 
 function Editor(props) {
   if (!props.currentNote)
@@ -78,15 +79,12 @@ function Editor(props) {
       <Container>
         <Row>
           <Col>
-            <div className="app-main" style={{ minHeight: "40rem" }}>
+            <div className="app-main" style={{ minHeight: "120vh" }}>
               <div className="app-main-note-edit">
                 {editing ? (
                   <div ref={textareaRef} onClick={handleOutsideClick}>
-                    <textarea
-                      rows={1}
+                    <TextareaAutosize
                       style={{
-                        width: "400px",
-                        height: "100px",
                         backgroundColor: "transparent",
                         color: "darkslategray",
                         width: "100%",
@@ -100,10 +98,11 @@ function Editor(props) {
                     />
                     {textAreas.map((label, index) => (
                       <div key={index}>
-                        <textarea
+                        <TextareaAutosize
                           onKeyDown={(e) =>
                             handleKeyPress(e, label === "" ? index : undefined)
                           }
+                          // rows={1}
                           style={{
                             backgroundColor: "transparent",
                             color: "darkslategray",
@@ -122,27 +121,26 @@ function Editor(props) {
                     <div
                       onClick={toggleEditMode}
                       className="app-main-note-preview"
-                      style={{ whiteSpace: "pre-line", color: "darkslategray" }}
+                      style={{
+                        whiteSpace: "pre-line",
+                        color: "darkslategray",
+                        display: "flex",
+                        // backgroundColor: "red",
+                      }}
                     >
-                      <h1 className="preview-title">
+                      <h1
+                        style={{ fontSize: "2rem", backgroundColor: "blue" }}
+                        className="preview-title"
+                      >
                         {props.currentNote.title}
                       </h1>
                       <ReactMarkdown className="markdown-preview">
                         {`${props.currentNote.body}\n${textAreas.join("\n")}`}
                       </ReactMarkdown>
                     </div>
-                    {/* <div>
-              <textarea placeholder="outside text area" />
-            </div> */}
                   </>
                 )}
               </div>
-              {/* <div className="app-main-note-preview">
-        <h1 className="preview-title">{props.currentNote.title}</h1>
-        <ReactMarkdown className="markdown-preview">
-          {props.currentNote.body}
-        </ReactMarkdown>
-      </div> */}
             </div>
           </Col>
         </Row>
@@ -150,120 +148,4 @@ function Editor(props) {
     </>
   );
 }
-
-// export default Editor;
-
-// import ReactMarkdown from "react-markdown";
-// import React, { useState, useRef, useCallback, useEffect } from "react";
-
-// function Editor(props) {
-//   if (!props.currentNote)
-//     return <div className="no-active-note">No Active Note</div>;
-
-//   const [editing, setEditing] = useState(false);
-//   const [editedContent, setEditedContent] = useState(props.currentNote.body);
-//   const textareaRef = useRef(null);
-//   const [newTextarea, setNewTextarea] = useState(""); // Track the content of the new textarea
-
-//   const textAreas = props.currentNote.textAreas;
-//   const setTextAreas = props.updateTextarea;
-
-//   useEffect(() => {
-//     if (editing) {
-//       document.addEventListener("click", handleOutsideClick);
-//     } else {
-//       document.removeEventListener("click", handleOutsideClick);
-//     }
-
-//     return () => {
-//       document.removeEventListener("click", handleOutsideClick);
-//     };
-//   }, [editing]);
-
-//   const handleOutsideClick = (e) => {
-//     if (textareaRef.current && !textareaRef.current.contains(e.target)) {
-//       // props.updateNote(editedContent);
-//       setEditing(false);
-//     }
-//   };
-
-//   const toggleEditMode = () => {
-//     setEditing(!editing);
-//   };
-
-//   const handleKeyPress = (e, index) => {
-//     if (e.key === "Backspace") {
-//       setTextAreas(textAreas.filter((label, i) => i !== index));
-//     }
-
-//     if (e.key === "Enter") {
-//       // Create a new textarea with the content of the newTextarea state
-//       setTextAreas([...textAreas, newTextarea]);
-
-//       // Clear the newTextarea state to start with an empty textarea next time
-//       setNewTextarea("");
-
-//       e.preventDefault(); // Prevent the default Enter key behavior (newline)
-//     }
-//   };
-
-//   const handleTextareaChange = (e, index) => {
-//     setTextAreas(
-//       textAreas.map((txt, i) => (i === index ? e.target.value : txt))
-//     );
-//   };
-
-//   const lastTextarea = useCallback((textarea) => {
-//     if (textarea) textarea.focus();
-//   }, []);
-
-//   return (
-//     <div className="app-main">
-//       <div className="app-main-note-edit">
-//         {editing ? (
-//           <div ref={textareaRef} onClick={handleOutsideClick}>
-//             <textarea
-//               rows={1}
-//               style={{ width: "400px", height: "100px" }}
-//               placeholder="Write your note here..."
-//               value={props.currentNote.body}
-//               onChange={(e) => {
-//                 props.updateNote(e.target.value);
-//               }}
-//               onKeyDown={handleKeyPress}
-//             />
-//             {textAreas.map((label, index) => (
-//               <div key={index}>
-//                 <textarea
-//                   onKeyDown={(e) =>
-//                     handleKeyPress(e, label === "" ? index : undefined)
-//                   }
-//                   ref={lastTextarea}
-//                   onChange={(e) => handleTextareaChange(e, index)}
-//                   value={label}
-//                 />
-//               </div>
-//             ))}
-//           </div>
-//         ) : (
-//           // <div onClick={toggleEditMode}>{props.currentNote.body}</div>
-//           <>
-//             <div
-//               onClick={toggleEditMode}
-//               className="app-main-note-preview"
-//               style={{ whiteSpace: "pre-line" }}
-//             >
-//               <h1 className="preview-title">{props.currentNote.title}</h1>
-//               <ReactMarkdown className="markdown-preview">
-//                 {`luka`}
-//                 {/* {`${props.currentNote.body}\n${textAreas.join("\n")}`} */}
-//               </ReactMarkdown>
-//             </div>
-//           </>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
 export default Editor;
